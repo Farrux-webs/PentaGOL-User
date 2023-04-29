12f291b8cab534dd1ab03fc378dad85196457aca
SanjarUsmonov <108736016+SanjarUsmonov@users.noreply.github.com>
2023-04-29 20:07:42 +0500

    added new file and table component

--- src/components/TableMatch/index.jsx
+++ src/components/TableMatch/index.jsx
@@ -7,7 +7,7 @@ const Table = () => {
     <div className="container">
       <div className="table flex">
         <NavLink
-          to="/dver"
+          to="/seria"
           className={({ isActive }) =>
             isActive ? " active_table flex" : "table-liga flex"
           }
@@ -15,30 +15,189 @@ const Table = () => {
           <img src="../../src/assets/icons/Seria-A.svg" alt="" /> Italiya.
           Seriya A
         </NavLink>
-        <NavLink to="/sdcd"    className={({ isActive }) =>
+        <NavLink
+          to="/apl"
+          className={({ isActive }) =>
             isActive ? " active_table flex" : " table-liga flex"
-          }>
-          <img src="../../src/assets/icons/APL.svg" alt="" /> Italiya. Seriya A
+          }
+        >
+          <img src="../../src/assets/icons/APL.svg" alt="" /> Premier Liga
         </NavLink>
-        <NavLink to="/wdcwe" className={({ isActive }) =>
+        <NavLink
+          to="/laliga"
+          className={({ isActive }) =>
             isActive ? " active_table flex" : " table-liga flex"
-          }>
-          <img src="../../src/assets/icons/Laliga.svg" alt="" /> Italiya. Seriya
-          A
+          }
+        >
+          <img src="../../src/assets/icons/Laliga.svg" alt="" /> LaLiga
+          
         </NavLink>
-        <NavLink to="/wfwef" className={({ isActive }) =>
+        <NavLink
+          to="/bundesliga"
+          className={({ isActive }) =>
             isActive ? " active_table flex" : " table-liga flex"
-          }>
-          <img src="../../src/assets/icons/Bundesliga.svg" alt="" /> Italiya.
-          Seriya A
+          }
+        >
+          <img src="../../src/assets/icons/Bundesliga.svg" alt="" /> Bundesliga
         </NavLink>
-        <NavLink to="/dwf" className={({ isActive }) =>
+        <NavLink
+          to="/ligue"
+          className={({ isActive }) =>
             isActive ? " active_table flex" : " table-liga flex"
-          }>
-          <img src="../../src/assets/icons/FranceLiga.svg" alt="" /> Italiya.
-          Seriya A
+          }
+        >
+          <img src="../../src/assets/icons/FranceLiga.svg" alt="" /> Ligue 1 
         </NavLink>
       </div>
+ <div className="table-wrapper flex">
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ <div className="table-matches flex">
+        <div className="table-match flex">
+          <div className="first-club"></div>
+          <img src="" alt="" />
+          <h4>Barcelona</h4>
+        </div>
+        <div className="score flex">
+          <h2>0</h2>
+          <h2>:</h2>
+          <h2>0</h2>
+        </div>
+        <div className="table-match flex">
+          <div className="second-club"></div>
+          <img src="" alt="" />
+          <h4>Liverpool</h4>
+        </div>
+        <span>11.11.2021 23:59</span>
+      </div>
+ 
+ 
+      </div>
+
     </div>
   );
 };
