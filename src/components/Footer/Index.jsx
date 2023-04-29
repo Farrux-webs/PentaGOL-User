150ae0e0961b5d3fe900a9afce71a12e65b2e936
SanjarUsmonov <108736016+SanjarUsmonov@users.noreply.github.com>
2023-04-29 16:59:27 +0500

    added footer with flex

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="footer">
        <div className="container">
-          <div className="footer-box">
+          <div className="footer-box flex">
             <div className="footer-log">
               <img src="../../../src/assets/images/Logo-Navbar.svg" alt="" />
             </div>
@@ -26,15 +26,15 @@ const Footer = () => {
             </div>
             <div className="footer-social-media">
               <div className="footer-third">
-                     <img src="" alt="" />
-                     <img src="" alt="" />
-                     <img src="" alt="" />
+                     <img src="../../src/assets/icons/Twitter.svg" alt="" />
+                     <img src="../../src/assets/icons/YouTube.svg" alt="" />
+                     <img src="../../src/assets/icons/Vkontakte (VK).svg" alt="" />
               </div>
               <div className="footer-forth">
-                     <img src="" alt="" />
-                     <img src="" alt="" />
-                     <img src="" alt="" />
-                     <img src="" alt="" />
+                     <img src="../../../src/assets/icons/Skype.svg" alt="" />
+                     <img src="../../../src/assets/icons/Instagram.svg" alt="" />
+                     <img src="../../../src/assets/icons/Facebook.svg" alt="" />
+                     <img src="../../../src/assets/icons/Telegram.svg" alt="" />
               </div>
             </div>
           </div>
