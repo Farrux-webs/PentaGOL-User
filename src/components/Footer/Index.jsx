d986e7f3f1c8611a11f9dd67566c5f557d381bc0
SanjarUsmonov <108736016+SanjarUsmonov@users.noreply.github.com>
2023-04-29 17:36:58 +0500

    add css

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className="footer">
        <div className="container">
-          <div className="footer-box flex">
+          <div className="footer-box">
             <div className="footer-log">
               <img src="../../../src/assets/images/Logo-Navbar.svg" alt="" />
             </div>
@@ -18,7 +18,6 @@ const Footer = () => {
               <NavLink to="" className="footer-link">tennis</NavLink>
               <NavLink to="" className="footer-link">cybersport</NavLink>
             </div>
-            
             <div className="footer-menu">
               <NavLink to="" className="footer-link">main</NavLink>
               <NavLink to="" className="footer-link">match center</NavLink>
@@ -26,17 +25,24 @@ const Footer = () => {
               <NavLink to="" className="footer-link">tennis</NavLink>
               <NavLink to="" className="footer-link">cybersport</NavLink>
             </div>
-            <div className="footer-social-media">
-              <div className="footer-third">
-                     <img src="../../src/assets/icons/Twitter.svg" alt="" />
-                     <img src="../../src/assets/icons/YouTube.svg" alt="" />
-                     <img src="../../src/assets/icons/Vkontakte (VK).svg" alt="" />
+            <div className="footer-menu">
+              <NavLink to="" className="footer-link">main</NavLink>
+              <NavLink to="" className="footer-link">match center</NavLink>
+              <NavLink to="" className="footer-link">football</NavLink>
+              <NavLink to="" className="footer-link">tennis</NavLink>
+              <NavLink to="" className="footer-link">cybersport</NavLink>
+            </div>
+            <div className="footer-social-media flex">
+              <div className="footer-third flex">
+                     <img className='icon' src="../../src/assets/icons/Twitter.svg" alt="" />
+                     <img className='icon' src="../../src/assets/icons/YouTube.svg" alt="" />
+                     <img className='icon' src="../../src/assets/icons/Vkontakte (VK).svg" alt="" />
               </div>
-              <div className="footer-forth">
-                     <img src="../../../src/assets/icons/Skype.svg" alt="" />
-                     <img src="../../../src/assets/icons/Instagram.svg" alt="" />
-                     <img src="../../../src/assets/icons/Facebook.svg" alt="" />
-                     <img src="../../../src/assets/icons/Telegram.svg" alt="" />
+              <div className="footer-forth flex">
+                     <img className='icon' src="../../../src/assets/icons/Skype.svg" alt="" />
+                     <img className='icon' src="../../../src/assets/icons/Instagram.svg" alt="" />
+                     <img className='icon' src="../../../src/assets/icons/Facebook.svg" alt="" />
+                     <img className='icon' src="../../../src/assets/icons/Telegram.svg" alt="" />
               </div>
             </div>
           </div>
