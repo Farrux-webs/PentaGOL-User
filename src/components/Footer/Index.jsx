f3af1177e90d967d8e4a142232da2ea75ec4106c
SanjarUsmonov <108736016+SanjarUsmonov@users.noreply.github.com>
2023-04-29 16:51:31 +0500

    added new style

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -1,4 +1,3 @@
-React
 import { React } from 'react';
 import "./style.scss"
 
@@ -6,10 +5,10 @@ import "./style.scss"
 const Footer = () => {
   return (
     <div className="footer">
-       <div className="footer-container">
+       <div className="container">
           <div className="footer-box">
             <div className="footer-log">
-              <img src="" alt="" />
+              <img src="../../../src/assets/images/Logo-Navbar.svg" alt="" />
             </div>
             <div className="footer-menu">
               <a href="">main</a>
